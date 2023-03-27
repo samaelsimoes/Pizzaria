089782b690cc8782f9f809cfd1470c4208489e19
samaelsimoes <samael.simoes@gmail.com>
2023-03-26 23:00:44 -0300

    Finalizado projeto da pizzaria, categoria, menu e rotas

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,11 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
 
 import { api } from '../services/apiClient';
 
 import { destroyCookie, setCookie, parseCookies } from 'nookies'
 import Router from 'next/router';
+
+import { toast } from 'react-toastify'
 
 
 type AuthContextData = {
@@ -11,6 +13,7 @@ type AuthContextData = {
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
   signOut: () => void;
+  signUp: (credentials: SignUpProps) => Promise<void>;
 }
 
 type UserProps = {
@@ -20,6 +23,12 @@ type UserProps = {
 }
 
 type SignInProps = {
+  email: string;
+  password: string;
+}
+
+type SignUpProps = {
+  name: string;
   email: string;
   password: string;
 }
@@ -43,6 +52,31 @@ export function signOut(){
 export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+
+    // tentar pegar algo no cookie
+    const { '@nextauth.token': token } = parseCookies();
+
+    if(token){
+      api.get('/me').then(response => {
+        const { id, name, email } = response.data;
+
+        setUser({
+          id,
+          name,
+          email
+        })
+
+      })
+      .catch(() => {
+        //Se deu erro deslogamos o user.
+        signOut();
+      })
+    }
+
+
+  }, [])
 
   async function signIn({ email, password }: SignInProps){
     try{
@@ -68,17 +102,40 @@ export function AuthProvider({ children }: AuthProviderProps){
       //Passar para proximas requisiçoes o nosso token
       api.defaults.headers['Authorization'] = `Bearer ${token}`
 
+      toast.success('Logado com sucesso!')
+
       //Redirecionar o user para /dashboard
       Router.push('/dashboard')
 
 
     }catch(err){
+      toast.error("Erro ao acessar!")
       console.log("ERRO AO ACESSAR ", err)
     }
   }
 
+
+  async function signUp({ name, email, password}: SignUpProps){
+    try{
+      
+      const response = await api.post('/users', {
+        name,
+        email,
+        password
+      })
+
+      toast.success("Conta criada com sucesso!")
+
+      Router.push('/')
+
+    }catch(err){
+      toast.error("Erro ao cadastrar!")
+      console.log("erro ao cadastrar ", err)
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
       {children}
     </AuthContext.Provider>
   )
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,3 +1,5 @@
+import { useState, FormEvent, useContext  } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image';
 import styles from '../../../styles/home.module.scss';
@@ -7,9 +9,42 @@ import logoImg from '../../../public/logo.svg';
 import { Input } from '../../components/ui/Input'
 import { Button } from '../../components/ui/Button'
 
+import { AuthContext } from '../../contexts/AuthContext'
+import { toast } from 'react-toastify'
+
 import Link from 'next/link';
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent){
+    event.preventDefault();
+
+    if(name === '' || email === '' || password === ''){
+      toast.error("Preencha todos os campos")
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      name,
+      email,
+      password
+    }
+
+    await signUp(data)
+
+    setLoading(false);
+
+  }
+
   return (
     <>
     <Head>
@@ -21,25 +56,31 @@ export default function SignUp() {
       <div className={styles.login}>
         <h1>Criando sua conta</h1>
 
-        <form>
+        <form onSubmit={handleSignUp}>
           <Input
             placeholder="Digite seu nome"
             type="text"
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
           />
 
           <Input
             placeholder="Digite seu email"
             type="text"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
 
           <Input
             placeholder="Sua senha"
             type="password"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
           />
           
           <Button
             type="submit"
-            loading={false}
+            loading={loading}
           >
             Cadastrar
           </Button>
